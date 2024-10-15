8e8f09ce3b66c848c90b90efa746131b874e25be
Frisaf <fridasafsten@gmail.com>
2024-10-15 15:14:51 +0200

    started implementing light mode toggle

--- docs/script.js
+++ docs/script.js
@@ -5,3 +5,14 @@ function openNav() {
 function closeNav() {
     document.getElementById("sidePanel").style.width = "0";
 }
+
+
+const switchButton = document.querySelector("#switch")
+switchButton.addEventListener("click", (event) => {
+
+    event.preventDefault();
+    const theme = document.documentElement.getAttribute("data-theme");
+    const newTheme = theme === "light" ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+})
