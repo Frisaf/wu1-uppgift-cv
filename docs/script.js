865c8ea2606996df6ebc82254b533cc26cd32044
Frisaf <fridasafsten@gmail.com>
2024-10-17 08:18:12 +0200

    fixed light mode

--- docs/script.js
+++ docs/script.js
@@ -16,3 +16,23 @@ switchButton.addEventListener("click", (event) => {
     document.documentElement.setAttribute("data-theme", newTheme);
     localStorage.setItem("theme", newTheme);
 })
+
+const toggle = document.querySelector(".toggle");
+const html = document.querySelector("html")
+
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const defaultTheme = prefersDarkMode ? "dark" : "ligth";
+const preferredTheme = localStorage.getItem("theme")
+
+if (!preferredTheme) {
+    localStorage.setItem("theme", defaultTheme);
+}
+
+html.dataset.theme = preferredTheme || defaultTheme;
+
+toggle.addEventListener("click", () => {
+    const isDarkTheme = localStorage.getItem("theme") === "dark";
+    const newTheme = isDarkTheme ? "light" : "dark"
+    localStorage.setItem("theme", newTheme);
+    html.dataset.theme = newTheme
+});
